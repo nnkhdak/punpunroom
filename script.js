e99f0ab6b38b9e51c3d4e49df6b7019f76dbc1fc
Hideaki NONAKA <h-nonaka@jiem.co.jp>
2020-06-18 01:35:01 +0900

    commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-$(function(){
+const USERS_API = "https://jsonplaceholder.typicode.com/users";
+
+async function callApi() {
+    const res = await window.fetch(USERS_API);
+    console.log(res);
+
+    const users = await res.json();
+    console.log(users);
+}
+
+$(function () {
     console.log('Hello');
+
+    callApi();
 });
