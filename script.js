75eecf6a2c162ec08030e24a9ab1dba5a40fa837
Hideaki NONAKA <h-nonaka@jiem.co.jp>
2020-06-19 22:35:22 +0900

    commit

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const USERS_API = "https://jsonplaceholder.typicode.com/users";
+const MENUS_API = "https://pun-pun-pun.github.io/punpunroom/menu.json";
 
 async function callApi() {
     const res = await window.fetch(USERS_API);
-    console.log(res);
-
     const users = await res.json();
     console.log(users);
 }
@@ -11,5 +10,53 @@ async function callApi() {
 $(function () {
     console.log('Hello');
 
-    callApi();
+    //    callApi();
+    codingNavTag('checkbox').then(tag => {
+        $('div div nav').append(tag);
+        console.log(tag);
+    });
 });
+
+async function codingNavTag(type) {
+    prefix = 'side';
+    if ('checkbox' == type) {
+        const result = document.createElement('ul');
+        const res = await window.fetch(MENUS_API);
+        const rows0 = await res.json();
+        rows0.forEach(row0 => {
+            const li0 = document.createElement('li');
+            result.appendChild(li0);
+
+            const lbl0 = document.createElement('label');
+            lbl0.innerHTML = row0.name;
+            lbl0.setAttribute('for', prefix + '_' + row0.name);
+            li0.appendChild(lbl0);
+
+            const input0 = document.createElement('input');
+            input0.setAttribute('id', prefix + '_' + row0.name);
+            input0.setAttribute('type', type);
+            input0.setAttribute('value', row0.name);
+            li0.appendChild(input0);
+
+            const ul1 = document.createElement('ul');
+            li0.appendChild(ul1);
+
+            const rows1 = row0.child;
+            rows1.forEach(row1 => {
+                console.log(row1);
+
+                const li1 = document.createElement('li');
+                ul1.appendChild(li1);
+
+                const a1 = document.createElement('a');
+                a1.innerHTML = row1.name;
+                if (row1.href && row1.href.length > 0) {
+                    a1.href = row0.name + '/' + row1.href;
+                }
+                li1.appendChild(a1);
+            });
+        });
+        return result;
+    }
+    return 'ddd';
+}
