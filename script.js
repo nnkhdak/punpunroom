c75560f184d28bfda43f14e0df4f0d91e03621b7
Hideaki NONAKA <h-nonaka@jiem.co.jp>
2020-06-19 23:04:18 +0900

    commit

--- script.js
+++ script.js
@@ -8,43 +8,100 @@ async function callApi() {
 }
 
 $(function () {
-    console.log('Hello');
+    // console.log('Hello');
+    // callApi();
 
-    //    callApi();
-    codingNavTag('checkbox').then(tag => {
-        $('div div nav').append(tag);
-        console.log(tag);
+    codingNavTag('radio', 'head').then(tag => {
+        const targetNode = document.querySelectorAll("header nav")[0];
+        targetNode.parentNode.replaceChild(tag, targetNode);
+    });
+
+    codingNavTag('checkbox', 'side').then(tag => {
+        const targetNode = document.querySelectorAll("div div nav")[0];
+        targetNode.parentNode.replaceChild(tag, targetNode);
     });
 });
 
-async function codingNavTag(type) {
-    prefix = 'side';
-    if ('checkbox' == type) {
-        const result = document.createElement('ul');
-        const res = await window.fetch(MENUS_API);
-        const rows0 = await res.json();
-        rows0.forEach(row0 => {
-            const li0 = document.createElement('li');
-            result.appendChild(li0);
+async function codingNavTag(type, prefix) {
+    const res = await window.fetch(MENUS_API);
+    const rows0 = await res.json();
+    const result = document.createElement('nav');
 
+    if ('checkbox' == type) {
+
+        // nav ul
+        const ul0 = document.createElement('ul');
+        result.appendChild(ul0);
+
+        rows0.forEach(row0 => {
+            // nav ul li
+            const li0 = document.createElement('li');
+            ul0.appendChild(li0);
+
+            // nav ul li label
             const lbl0 = document.createElement('label');
             lbl0.innerHTML = row0.name;
             lbl0.setAttribute('for', prefix + '_' + row0.name);
             li0.appendChild(lbl0);
 
+            // nav ul li input
             const input0 = document.createElement('input');
             input0.setAttribute('id', prefix + '_' + row0.name);
             input0.setAttribute('type', type);
             input0.setAttribute('value', row0.name);
             li0.appendChild(input0);
 
+            // nav ul li ul
             const ul1 = document.createElement('ul');
             li0.appendChild(ul1);
 
             const rows1 = row0.child;
             rows1.forEach(row1 => {
-                console.log(row1);
 
+                // nav ul li ul
+                const li1 = document.createElement('li');
+                ul1.appendChild(li1);
+
+                // nav ul li ul a
+                const a1 = document.createElement('a');
+                a1.innerHTML = row1.name;
+                if (row1.href && row1.href.length > 0) {
+                    a1.href = row0.name + '/' + row1.href;
+                }
+                li1.appendChild(a1);
+            });
+        });
+        return result;
+    }
+
+    if ('radio' == type) {
+
+        // nav ul
+        const ul0 = document.createElement('ul');
+        result.appendChild(ul0);
+
+        // nav div
+        const div0 = document.createElement('div');
+        result.appendChild(div0);
+
+        rows0.forEach(row0 => {
+
+            // nav div input
+            const input0 = document.createElement('input');
+            input0.setAttribute('id', prefix + '_' + row0.name);
+            input0.setAttribute('type', type);
+            input0.setAttribute('name', prefix);
+            input0.setAttribute('value', row0.name);
+            div0.appendChild(input0);
+
+            // nav div ul
+            const ul1 = document.createElement('ul');
+            div0.appendChild(ul1);
+
+            const rows1 = row0.child;
+            rows1.forEach(row1 => {
+
+                // nav div ul li
                 const li1 = document.createElement('li');
                 ul1.appendChild(li1);
 
@@ -55,6 +112,17 @@ async function codingNavTag(type) {
                 }
                 li1.appendChild(a1);
             });
+
+            // nav ul li
+            const li0 = document.createElement('li');
+            ul0.appendChild(li0);
+
+            // nav ul li label
+            const lbl0 = document.createElement('label');
+            lbl0.innerHTML = row0.name;
+            lbl0.setAttribute('for', prefix + '_' + row0.name);
+            li0.appendChild(lbl0);
+
         });
         return result;
     }
