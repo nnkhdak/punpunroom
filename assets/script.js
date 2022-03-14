9ec6e1cfba0573c11cfd179552693c2a0f90c028
Hideaki NONAKA <h-nonaka@jiem.co.jp>
2022-03-14 11:21:31 +0900

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const MENUS_API = "https://pun-pun-pun.github.io/punpunroom/menu.json";
+const MENUS_API = "https://nnkhdak.github.io/punpunroom/menu.json";
 
 // header coding
 document.querySelectorAll('header')
