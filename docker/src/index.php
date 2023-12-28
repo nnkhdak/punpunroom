249305cc3cc7711b724ac71a76fac10b3ed38933
h-nonaka-jiem <h-nonaka@jiem.co.jp>
2023-12-28 23:59:18 +0900

    commit

--- docker/src/index.php
+++ docker/src/index.php
@@ -4,7 +4,7 @@ echo 'helloworld';
 $mysqli = new mysqli('db', 'root', 'pass', 'mysql');
 if($mysqli->connect_error) {
     echo '接続失敗'.PHP_EOL;
-    exit();
 } else {
     echo '接続成功'.PHP_EOL;
 }
+exit();
