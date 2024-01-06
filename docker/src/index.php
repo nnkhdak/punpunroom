41f85492d838adcdd9dfa15a105cf47ba863a57d
h-nonaka-jiem <h-nonaka@jiem.co.jp>
2024-01-06 19:53:34 +0900

    commit

--- docker/src/index.php
+++ docker/src/index.php
@@ -10,25 +10,29 @@ try {
 	$dto = array('id' => 2);							// DTO(DataTransferObject)を生成する
 	$dto = array_merge($dto, array('age' => 'hehe'));	// 不要項目を追加する
 	$dao->loadByKey($transaction, $dto);				// 主キーを用いてDTOにデータを読み込む
-echo json_encode($dto);
-echo "\n<br>";
+	echo json_encode($dto) . "\n<br>";
+	assert('{"id":2,"age":"hehe","name":"bbb"}' === json_encode($dto));
 
 	$dao = DaoFactory::newInstance('double_key');		// DAO(DataAccessObject)を生成する
 	$dto = array('key1' => 'aaa', 'key2' => 1);			// DTO(DataTransferObject)を生成する
 	$dto = array_merge($dto, array('age' => 'hehe'));	// 不要項目を追加する
 	$dao->loadByKey($transaction, $dto);				// 主キーを用いてDTOにデータを読み込む
-echo json_encode($dto);
-echo "\n<br>";
+	echo json_encode($dto) . "\n<br>";
+	assert('{"key1":"aaa","key2":1,"age":"hehe","val1":"@aaa@","val2":111}' === json_encode($dto));
 
 	$dto = array('key1' => 'aaa', 'key2' => null);		// DTO(DataTransferObject)を生成する
 	$dto = array_merge($dto, array('age' => 'hehe'));	// 不要項目を追加する
 	$it = $dao->read($transaction, $dto);				// DTOを用いて検索する
+	$rows = array();
 	while ($it->valid()) {
 		$row = $it->current();
-echo json_encode($row);
-echo "\n<br>";
+		$rows[] = $row;
 		$it->next();
 	}
+	echo json_encode($rows[0]) . "\n<br>";
+	assert('{"key1":"aaa","key2":1,"val1":"@aaa@","val2":111}' === json_encode($rows[0]));
+	echo json_encode($rows[1]) . "\n<br>";
+	assert('{"key1":"aaa","key2":2,"val1":"*aaa*","val2":222}' === json_encode($rows[1]));
 
 	$transaction->commit();							// 正常終了したのでcommitする
 } catch (\Exception $e) {
