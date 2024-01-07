7fdb92d6b019413818a85b5d9e77da1fe317d79e
h-nonaka-jiem <h-nonaka@jiem.co.jp>
2024-01-07 15:39:02 +0900

    commit

--- docker/src/dao/DaoFactory.php
+++ docker/src/dao/DaoFactory.php
@@ -6,7 +6,33 @@ require_once('dao/Transaction.php');
 
 class DaoFactory {
 
+	private static function calcMapping() {
+		$path = sprintf('./%s.json', __CLASS__);
+		$path = str_replace('\\', '/', $path);
+		if (!file_exists($path)) {
+			return array();
+		}
+
+		$path = realpath($path);
+		$map = file_get_contents($path);
+		$map = json_decode($map, true);
+		return $map;
+	}
+
 	public static function newInstance($keyword) {
+		$map = self::calcMapping();
+		if (!empty($map)) {
+			if (array_key_exists($keyword, $map)) {
+				$fqcn = $map[$keyword];
+				$path = sprintf('%s.php', $fqcn);
+				$path = str_replace('\\', '/', $path);
+				require_once($path);
+
+				$impl = new $fqcn;
+				return $impl;
+			}
+		}
+
 		if ($keyword === 'person') {
 			return new \dao\PersonDao();
 		}
