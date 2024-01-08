d5a4abca0e91410ff893e9d6b24c76f02190a4d9
h-nonaka-jiem <h-nonaka@jiem.co.jp>
2024-01-08 18:10:58 +0900

    commit

--- docker/src/dao/PersonDao.php
+++ docker/src/dao/PersonDao.php
@@ -14,9 +14,9 @@ class PersonDao implements Dao {
 		$row2 = json_decode('{"id":2,"name":"bbb"}', true);
 		$row3 = json_decode('{"id":3,"name":"ccc"}', true);
 		$row9 = json_decode('{"id":9,"name":"aaa"}', true);
-		$rows = array($row1, $row2, $row3, $row9);
+		$rows = array($row9, $row3, $row2, $row1);
 		if (!empty($dto)) {
-			$rows = array($row1, $row9);
+			$rows = array($row9, $row1);
 		}
 		$obj = new ArrayObject($rows);
 		$it = $obj->getIterator();
